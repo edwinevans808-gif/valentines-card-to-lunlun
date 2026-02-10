d1d31b99d2cd67e069a413e97ca8cd3aef274447
Edwinevans808-gif <Edwinevans808@gmail.com>
2026-02-10 20:03:42 +0530

    Final version with jumping button and fixed text

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const noBtn = document.querySelector(".no-btn");
 const yesBtn = document.querySelector(".btn[alt='Yes']");
 
 const title = document.getElementById("letter-title");
-const catImg = document.getElementById("letter-me");
+const catImg = document.getElementById("letter-cat");
 const buttons = document.getElementById("letter-buttons");
 const finalText = document.getElementById("final-text");
 
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "Lets gooo!!";
 
     catImg.src = "me_thumbs_up.gif";
 
